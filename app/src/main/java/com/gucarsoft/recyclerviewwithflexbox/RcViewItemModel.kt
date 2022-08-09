f2becbeb0b943101d51c70d71bb9f4fbad5a278b
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-09 15:04:34 +0300

    rc view item model created

---
+++ app/src/main/java/com/gucarsoft/recyclerviewwithflexbox/RcViewItemModel.kt
@@ -0,0 +1,12 @@
+package com.gucarsoft.recyclerviewwithflexbox
+
+class RcViewItemModel {
+
+    var title:String ?=null
+    var content:String ?=null
+
+    constructor(title: String?, content: String?) {
+        this.title = title
+        this.content = content
+    }
+}
